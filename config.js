5d1a904a557137eef5795e3c4eaae782fd84e0c6
Cristian Ricartez <cristian.ricartez@hotmail.com>
2021-10-14 03:22:10 -0300

    Agregada pantalla de plan dietario semanal. Solo funcionando con patología celiaquia

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const LOCAL_IP = "192.168.0.236"
 const BACKEND_BASE_URL = `http://${LOCAL_IP}:8000`
+const BACKEND_PLAN_BASE_URL = `http://${LOCAL_IP}:3050`
 let config = {
     backendURLs: {
         patologiasList: `${BACKEND_BASE_URL}/api/patologias/list`,
         patologiasUsuariosList: `${BACKEND_BASE_URL}/api/usuarios_patologias/list`,
-        patologiasUsuariosCreate: `${BACKEND_BASE_URL}/api/usuarios_patologias/create`
+        patologiasUsuariosCreate: `${BACKEND_BASE_URL}/api/usuarios_patologias/create`,
+        planDietario: `${BACKEND_PLAN_BASE_URL}/plan`
     },
     currentUser: {
         id:1,
