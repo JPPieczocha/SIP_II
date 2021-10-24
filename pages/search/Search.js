0752625347837be177ca50f05110fd48d7508161
Matias Pinto <mati_d_pinto@hotmail.com>
2021-10-24 17:52:26 -0300

    Search added

--- pages/search/Search.js
+++ pages/search/Search.js
@@ -1,13 +1,49 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, TextInput, Modal } from 'react-native';
 
+import FoodItem from '../common/components/FoodItem/FoodItem';
 import styles from './Styles';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import logo from '../../assets/logo.jpeg';
 
 const Search = ({navigation}) => {
+
+    const filterModal = () => {
+        return(
+            <Modal
+            
+            
+            
+            
+            >
+
+
+            </Modal>
+        )
+    }
+
+
+
+
     return (
-        <View>
-            <Text>Search</Text>
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+                <Image style={styles.headerLogo} source={logo} />;
+                <Text style={styles.headerTitle}>¿Buscabas algo?</Text>
+            </View>
+
+            <View style={styles.mainIput}>
+                <TextInput style={styles.input} placeholder={'Escriba aquí'}></TextInput>
+                <Text style={styles.iconFilter}>F</Text>
+            </View>
+
+            <View style={styles.main}>
+
+            </View>
+
+            {filterModal()}
+
         </View>
     )
 };
--- pages/search/Styles.js
+++ pages/search/Styles.js
@@ -1,7 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container:{
 
+    },
+    header:{
+
+    },
+    headerLogo:{
+
+    },
+    headerTitle: {
+        fontSize: 24,
+        fontFamily: 'SimplyDiet'
+    },
+    mainIput:{
+
+    },
+    input:{
+
+    },
+    iconFilter:{
+
+    },
+    main:{
+        
+    }
 });
 
 export default styles;
