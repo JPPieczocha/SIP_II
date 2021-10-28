5cad9e5f7a7fa4fd196845183a6e7b1930d452e3
Cristian Ricartez <cristian.ricartez@hotmail.com>
2021-10-27 23:14:28 -0300

    (Mod): modificado popup de guardado exitoso al actualizar patologías. También agregado popup de advertencia si se entra al plan dietario sin patologias cargadas

--- pages/planDietario/Styles.js
+++ pages/planDietario/Styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import colors from '../common/colors';
 
 const StylePlanDietario = StyleSheet.create({
     container:{
@@ -102,6 +103,53 @@ const StylePlanDietario = StyleSheet.create({
         right:6, 
         bottom: 4, 
         fontSize:11
+    },
+
+    modal_filter:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: 'rgba(0,0,0, 0.6)',
+    },
+
+    modal_container:{
+        width: '90%',
+        padding: 15,
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor:'#f2f2f2',
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: colors.secondaryv2
+    },
+
+    modal_title:{
+        fontFamily: 'SimplyDiet',
+        fontSize: 64,
+        color: colors.secondaryv2,
+        textAlign: 'center'
+    },
+
+    modal_description:{
+        fontSize: 20,
+        textAlign: 'center'
+    },
+
+    modal_button:{
+        alignSelf: 'center',
+        width: '75%',
+        height: 38,
+        borderRadius: 10,
+        marginVertical: 10,
+        padding: 8,
+        backgroundColor: colors.secondaryv2
+    },
+
+    modal_button_text:{
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+        textAlignVertical: 'center',
     }
 });
 
--- pages/profile/Styles.js
+++ pages/profile/Styles.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native';
+import colors from '../common/colors';
 
 const StyleProfile = StyleSheet.create({
     profile_title:{
@@ -14,6 +15,52 @@ const StyleProfile = StyleSheet.create({
         fontSize:22,
     },
 
+    modal_filter:{
+        flex: 1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: 'rgba(0,0,0, 0.6)',
+    },
+
+    modal_container:{
+        width: '90%',
+        padding: 15,
+        alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor:'#f2f2f2',
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: colors.secondaryv2
+    },
+
+    modal_title:{
+        fontFamily: 'SimplyDiet',
+        fontSize: 64,
+        color: colors.secondaryv2,
+        textAlign: 'center'
+    },
+
+    modal_description:{
+        fontSize: 20,
+        textAlign: 'center'
+    },
+
+    modal_button:{
+        alignSelf: 'center',
+        width: '75%',
+        height: 38,
+        borderRadius: 10,
+        marginVertical: 10,
+        padding: 8,
+        backgroundColor: colors.secondaryv2
+    },
+
+    modal_button_text:{
+        fontSize: 18,
+        color: 'white',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+    }
 });
 
 export default StyleProfile
