bce5bc17dbb14fb92ebbfc34cb354a35944b7d9a
Juan Pablo Pieczocha <70308282+JPPieczocha@users.noreply.github.com>
2021-11-06 17:15:24 -0300

    Emojis en ingredientes

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const NGROK_IP_USUARIOS = "http://dac3-200-114-203-215.ngrok.io"
-const NGROK_IP_PLAN_DIETARIO = "http://4ea3-200-114-203-215.ngrok.io"
+const NGROK_IP_USUARIOS = "http://2711-190-17-54-54.ngrok.io"
+const NGROK_IP_PLAN_DIETARIO = "https://1200-190-17-54-54.ngrok.io"
 let config = {
     backendURLs: {
         getUser:`${NGROK_IP_USUARIOS}/api/usuarios/findOne`,
@@ -7,7 +7,7 @@ let config = {
         patologiasUsuariosCreate: `${NGROK_IP_USUARIOS}/api/usuarios_patologias/create`,
         patologiasUsuariosDelete: `${NGROK_IP_USUARIOS}/api/usuarios_patologias/deleteByIdUsuario_IdPatologia`,
         patologiasUsuariosGet: `${NGROK_IP_USUARIOS}/api/usuarios_patologias/findPatologiasByIdUsuario`,
-        planDietario: `${NGROK_IP_PLAN_DIETARIO}/plan`
+        planDietario: `${NGROK_IP_PLAN_DIETARIO}/generate_plan_diario`
     },
     loggedUser:{
         id:1
